e25f146c222618108c0e2b15cd320302892248a0
Milkuuu <snsd0220@gmail.com>
2024-05-23 16:27:28 +0800

    變數跟檔名修改

--- script/controllers/mainController.js
+++ script/controllers/mainController.js
@@ -2,7 +2,7 @@ app.controller('MainController', function($scope, dataService) {
     $scope.zones = []; // 行政區域
     $scope.selectedZone = '甲仙區'; // 預設行政區域
     $scope.selectedSite = null; // 選擇的景點
-    $scope.isModalOpen = false; // 是否開啟景點資訊視窗
+    $scope.isSiteInfoOpen = false; // 是否開啟景點資訊視窗
     $scope.data = []; // 景點資料
     $scope.popularZones = ['苓雅區', '三民區', '甲仙區', '大樹區']; // 熱門行政區域
     $scope.currentZoneSites = []; // 當前行政區域的景點
@@ -19,7 +19,7 @@ app.controller('MainController', function($scope, dataService) {
     $scope.updateZone = function() { // 更新當前行政區域的景點
         $scope.currentZoneSites = $scope.data.filter(site => site.Zone === $scope.selectedZone);
         $scope.setPage(1);
-        $scope.isModalOpen = false;
+        $scope.isSiteInfoOpen = false;
     };
 
     $scope.selectZone = function(zone) { // 選擇行政區域
@@ -50,11 +50,11 @@ app.controller('MainController', function($scope, dataService) {
     };
 
     $scope.showSiteInfo = function(site) {
-        $scope.isModalOpen = true;
+        $scope.isSiteInfoOpen = true;
         $scope.selectedSite = site;
     };
 
     $scope.closeModal = function() {
-        $scope.isModalOpen = false;
+        $scope.isSiteInfoOpen = false;
     };
 });
